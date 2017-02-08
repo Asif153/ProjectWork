a59be9c785cfd04a82b4b779f05a6e7d631e7db6
P15180408 <P15180408@GWHXX583WIN007W>
2017-02-08 10:13:49 +0000

    creation of clsAddress and test class

---
+++ Test Framework/tstAddress.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Class_Library;
+
+namespace Test_Framework
+{
+    [TestClass]
+    public class tstAddress
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of our class clsAddress
+            clsAddress AnAdress = new clsAddress();
+            //check to see class is not null
+            Assert.IsNotNull(AnAdress);
+        }
+
+
+
+
+    }
+
+
+
+}
